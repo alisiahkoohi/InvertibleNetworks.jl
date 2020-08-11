76fea22a1fb67838d6bbe0bb6b9705428d91e205
Ali Siahkoohi <alisk@gatech.edu>
2020-08-11 17:50:30 -0400

    3D—test_squeeze

--- src/utils/dimensionality_operations.jl
+++ src/utils/dimensionality_operations.jl
@@ -16,7 +16,7 @@ export squeeze, unsqueeze, wavelet_squeeze, wavelet_unsqueeze, tensor_split, ten
  *Input*:
 
  - `X`: 4D or 5D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize` (4D)
-    or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
+        or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
 
  - `pattern`: Squeezing pattern
 
@@ -33,7 +33,7 @@ export squeeze, unsqueeze, wavelet_squeeze, wavelet_unsqueeze, tensor_split, ten
  *Output*:
 
  - `Y`: Reshaped tensor of dimensions `nx/2` x `ny/2` x `n_channel*4` x `batchsize` (4D)
-    or `nx/2` x `ny/2` x `nz/2` x `n_channel*8` x `batchsize` (5D)
+        or `nx/2` x `ny/2` x `nz/2` x `n_channel*8` x `batchsize` (5D)
 
 
  See also: [`unsqueeze`](@ref), [`wavelet_squeeze`](@ref), [`wavelet_unsqueeze`](@ref)
@@ -126,7 +126,7 @@ end
  *Input*:
 
  - `Y`: 4D or 5D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize` (4D)
-    or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
+        or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
 
  - `pattern`: Squeezing pattern
 
@@ -140,7 +140,7 @@ end
  *Output*:
 
  - `X`: Reshaped tensor of dimensions `nx*2` x `ny*2` x `n_channel/4` x `batchsize` (4D)
-    or `nx*2` x `ny*2` x `nz*2` x `n_channel/8` x `batchsize` (5D)
+        or `nx*2` x `ny*2` x `nz*2` x `n_channel/8` x `batchsize` (5D)
 
  See also: [`squeeze`](@ref), [`wavelet_squeeze`](@ref), [`wavelet_unsqueeze`](@ref)
 """
@@ -239,19 +239,18 @@ function unsqueeze(X::AbstractArray{T,5}, Y::AbstractArray{T,5}; pattern="column
     return unsqueeze(X; pattern=pattern), unsqueeze(Y; pattern=pattern)
 end
 
-
-####################################################################################################
 # Squeeze and unsqueeze using the wavelet transform
 
 """
     Y = wavelet_squeeze(X; type=WT.db1)
 
- Perform a 1-level channelwise 2D wavelet transform of X and squeeze output of each
+ Perform a 1-level channelwise 2D or 3D wavelet transform of X and squeeze output of each
  transform into 4 channels (per 1 input channel).
 
  *Input*:
 
- - `X`: 4D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize`
+ - `X`: 4D or 5D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize` (4D)
+        or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
 
  - `type`: Wavelet filter type. Possible values are `WT.haar` for Haar wavelets,
     `WT.coif2`, `WT.coif4`, etc. for Coiflet wavelets, or `WT.db1`, `WT.db2`, etc.
@@ -260,36 +259,63 @@ end
 
  *Output*:
 
- - `Y`: Reshaped tensor of dimensions `nx/2` x `ny/2` x `n_channel*4` x `batchsize`
+ - `Y`: Reshaped tensor of dimensions `nx/2` x `ny/2` x `n_channel*4` x `batchsize` (4D)
+        or `nx/2` x `ny/2` x `nz/2` x `n_channel/8` x `batchsize` (5D)
 
  See also: [`wavelet_unsqueeze`](@ref), [`squeeze`](@ref), [`unsqueeze`](@ref)
 """
 function wavelet_squeeze(X::AbstractArray{T,4}; type=WT.db1) where T
+
+    # Dimensions
     nx_in, ny_in, nc_in, batchsize = size(X)
+
     nx_out = Int(round(nx_in/2))
     ny_out = Int(round(ny_in/2))
-    nc_out = Int(round(nc_in*4))
+    nc_out = nc_in*4
     Y = cuzeros(X, nx_out, ny_out, nc_out, batchsize)
+
     for i=1:batchsize
         for j=1:nc_in
-            Ycurr = dwt(X[:,:,j,i], wavelet(type), 1)
-            Y[:, :, (j-1)*4 + 1: j*4, i] = squeeze(reshape(Ycurr, nx_in, ny_in, 1, 1); pattern="patch")
+            Ycurr = dwt(X[:, :, j, i], wavelet(type), 1)
+            Y[:, :, (j-1)*4+1:j*4, i] = squeeze(reshape(Ycurr, nx_in, ny_in, 1, 1);
+                                                pattern="patch")
         end
     end
     return Y
 end
 
+function wavelet_squeeze(X::AbstractArray{T,5}; type=WT.db1) where T
+
+    # Dimensions
+    nx_in, ny_in, nz_in, nc_in, batchsize = size(X)
+
+    nx_out = Int(round(nx_in/2))
+    ny_out = Int(round(ny_in/2))
+    nz_out = Int(round(nz_in/2))
+    nc_out = nc_in*8
+    Y = cuzeros(X, nx_out, ny_out, nz_out, nc_out, batchsize)
+
+    for i=1:batchsize
+        for j=1:nc_in
+            Ycurr = dwt(X[:, :, :, j, i], wavelet(type), 1)
+            Y[:, :, :, (j-1)*8+1:j*8, i] = squeeze(reshape(Ycurr, nx_in, ny_in, nz_in,
+                                                   1, 1); pattern="patch")
+        end
+    end
+    return Y
+end
 
 """
     X = wavelet_unsqueeze(Y; type=WT.db1)
 
- Perform a 1-level inverse 2D wavelet transform of Y and unsqueeze output.
+ Perform a 1-level inverse 2D or 3D wavelet transform of Y and unsqueeze output.
  This reduces the number of channels by 4 and increases each spatial
  dimension by a factor of 2. Inverse operation of `wavelet_squeeze`.
 
  *Input*:
 
- - `Y`: 4D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize`
+ - `Y`: 4D or 5D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize` (4D)
+        or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
 
  - `type`: Wavelet filter type. Possible values are `haar` for Haar wavelets,
   `coif2`, `coif4`, etc. for Coiflet wavelets, or `db1`, `db2`, etc. for Daubechies
@@ -297,19 +323,24 @@ end
 
  *Output*:
 
- - `X`: Reshaped tensor of dimenions `nx*2` x `ny*2` x `n_channel/4` x `batchsize`
+ - `X`: Reshaped tensor of dimensions `nx*2` x `ny*2` x `n_channel/4` x `batchsize` (4D)
+        or `nx*2` x `ny*2` x `nz*2` x `n_channel/8` x `batchsize` (5D)
 
  See also: [`wavelet_squeeze`](@ref), [`squeeze`](@ref), [`unsqueeze`](@ref)
 """
 function wavelet_unsqueeze(Y::AbstractArray{T,4}; type=WT.db1) where T
+
+    # Dimensions
     nx_in, ny_in, nc_in, batchsize = size(Y)
-    nx_out = Int(round(nx_in*2))
-    ny_out = Int(round(ny_in*2))
+
+    nx_out = nx_in*2
+    ny_out = ny_in*2
     nc_out = Int(round(nc_in/4))
     X = cuzeros(Y, nx_out, ny_out, nc_out, batchsize)
+
     for i=1:batchsize
         for j=1:nc_out
-            Ycurr = unsqueeze(Y[:, :, (j-1)*4 + 1: j*4, i:i]; pattern="patch")[:, :, 1, 1]
+            Ycurr = unsqueeze(Y[:, :, (j-1)*4+1:j*4, i:i]; pattern="patch")[:, :, 1, 1]
             Xcurr = idwt(Ycurr, wavelet(type), 1)
             X[:, :, j, i] = reshape(Xcurr, nx_out, ny_out, 1, 1)
         end
@@ -317,23 +348,46 @@ function wavelet_unsqueeze(Y::AbstractArray{T,4}; type=WT.db1) where T
     return X
 end
 
+function wavelet_unsqueeze(Y::AbstractArray{T,5}; type=WT.db1) where T
 
-####################################################################################################
+    # Dimensions
+    nx_in, ny_in, nz_in, nc_in, batchsize = size(Y)
+
+    nx_out = nx_in*2
+    ny_out = ny_in*2
+    nz_out = nz_in*2
+    nc_out = Int(round(nc_in/8))
+    X = cuzeros(Y, nx_out, ny_out, nz_out, nc_out, batchsize)
+
+    for i=1:batchsize
+        for j=1:nc_out
+            Ycurr = unsqueeze(Y[:, :, :, (j-1)*8+1:j*8, i:i]; pattern="patch")[:, :, :,
+                                                                               1, 1]
+            Xcurr = idwt(Ycurr, wavelet(type), 1)
+            X[:, :, :, j, i] = reshape(Xcurr, nx_out, ny_out, nz_out, 1, 1)
+        end
+    end
+    return X
+end
+
+
 # Split and concatenate
 
 """
     Y, Z = tensor_split(X)
 
- Split 4D input tensor in half along the channel dimension. Inverse operation
+ Split 4D or 5D input tensor in half along the channel dimension. Inverse operation
  of `tensor_cat`.
 
  *Input*:
 
- - `X`: 4D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize`
+ - `X`: 4D or 5D input tensor of dimensions `nx` x `ny` x `n_channel` x `batchsize` (4D)
+        or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
 
  *Output*:
 
- - `Y`, `Z`: 4D output tensors, each of dimensions `nx` x `ny` x `n_channel/2` x `batchsize`
+ - `Y`, `Z`: output tensors of dimensions `nx` x `ny` x `n_channel/2` x `batchsize` (4D)
+             or `nx` x `ny` x `nz` x `n_channel/2` x `batchsize` (5D)
 
  See also: [`tensor_cat`](@ref)
 """
@@ -367,16 +421,18 @@ end
 """
     X = tensor_cat(Y, Z)
 
- Concatenate 4D input tensors along the channel dimension. Inverse operation
+ Concatenate 4D or 5D input tensors along the channel dimension. Inverse operation
  of `tensor_split`.
 
  *Input*:
 
- - `Y`, `Z`: 4D input tensors, each of dimensions `nx` x `ny` x `n_channel` x `batchsize`
+ - `Y`, `Z`: 4D or 5D input tensors of dimensions `nx` x `ny` x `n_channel` x `batchsize`
+            (4D) or `nx` x `ny` x `nz` x `n_channel` x `batchsize` (5D)
 
  *Output*:
 
- - `X`: 4D output tensor of dimensions `nx` x `ny` x `n_channel*2` x `batchsize`
+ - `X`: output tensor of dimensions `nx` x `ny` x `n_channel*2` x `batchsize` (4D)
+             or `nx` x `ny` x `nz` x `n_channel*2` x `batchsize` (5D)
 
  See also: [`tensor_split`](@ref)
 """
@@ -410,4 +466,10 @@ function tensor_cat(X::AbstractArray{T,1}, Y::AbstractArray{T,1}) where T
     end
 end
 
-tensor_cat(X::Tuple{AbstractArray{T,4}, AbstractArray{T,4}}) where T = tensor_cat(X[1], X[2])
+function tensor_cat(X::Tuple{AbstractArray{T,4}, AbstractArray{T,4}}) where T
+    return tensor_cat(X[1], X[2])
+end
+
+function tensor_cat(X::Tuple{AbstractArray{T,5}, AbstractArray{T,5}}) where T
+    return tensor_cat(X[1], X[2])
+end
