cb09193c717324f815feb0ba1c525f1d6e41cc64
Mathias Louboutin <mathias.louboutin@gmail.com>
2020-09-16 16:38:23 -0400

    parallelize CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,19 @@
 # Tests for invertible neural network module
 # Author: Philipp Witte, pwitte3@gatech.edu
 # Date: January 2020
+
 using InvertibleNetworks, Test
 
-test_suite = "all"   # "all", "layers" or "networks"
+const group = get(ENV, "GROUP", "all")
 
-layers = ["test_utils/test_objectives.jl",
+# utils
+utils = ["test_utils/test_objectives.jl",
           "test_utils/test_nnlib_convolution.jl",
           "test_utils/test_activations.jl",
-          "test_utils/test_squeeze.jl",
-          # Layers
-          "test_layers/test_residual_block.jl",
+          "test_utils/test_squeeze.jl"]
+
+# Layers
+layers = ["test_layers/test_residual_block.jl",
           "test_layers/test_flux_block.jl",
           "test_layers/test_householder_convolution.jl",
           "test_layers/test_coupling_layer_basic.jl",
@@ -25,14 +28,25 @@ layers = ["test_utils/test_objectives.jl",
           "test_layers/test_hyperbolic_layer.jl",
           "test_layers/test_actnorm.jl"]
 
+# Networks
 networks = ["test_networks/test_unrolled_loop.jl",
             "test_networks/test_generator.jl",
             "test_networks/test_glow.jl",
             "test_networks/test_hyperbolic_network.jl",
             "test_networks/test_conditional_hint_network.jl"]
 
-if test_suite == "all" || test_suite == "layers"
-    for t=layers
+# Utils
+if group == "all" || group == "utils"
+    for t in utils
+        @testset  "Test $t" begin
+            include(t)
+        end
+    end
+end
+
+# Layers
+if group == "all" || group == "layers"
+    for t in layers
         @testset  "Test $t" begin
             include(t)
         end
@@ -40,8 +54,8 @@ if test_suite == "all" || test_suite == "layers"
 end
 
 # Networks
-if test_suite == "all" || test_suite == "networks"
-    for t=networks
+if group == "all" || group == "networks"
+    for t in networks
         @testset  "Test $t" begin
             include(t)
         end
